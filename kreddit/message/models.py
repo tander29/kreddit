786e07e2760f0bcb6990e6663b9df893e1ffbcfa
TJ Hindman <thind131@gmail.com>
2019-04-09 09:18:37 -0400

    deleted message model

--- kreddit/message/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from kreddit.kredditor.models import Kredditor
-
-
-class Message(models.Model):
-    # to user
-    user = models.ForeignKey(Kredditor, on_delete=models.CASCADE, null=True)
-    body = models.CharField(max_length=500)
-    date_created = models.DateTimeField(auto_now_add=True)
