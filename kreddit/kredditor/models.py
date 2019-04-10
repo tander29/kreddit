7ed5a74a62e7c23dc26240d3746316f61b9c36c4
TJ Hindman <thind131@gmail.com>
2019-04-10 15:08:37 -0400

    added subkreddit following to models

--- kreddit/kredditor/models.py
+++ kreddit/kredditor/models.py
@@ -6,7 +6,6 @@ class Kredditor(models.Model):
     # How to add avatars?
     # Kreddit score field for total upvotes on posts/comments
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    following = models.ManyToManyField('self', symmetrical=False, blank=True)
 
     def __str__(self):
         return self.user.username
--- kreddit/subkreddit/models.py
+++ kreddit/subkreddit/models.py
@@ -4,8 +4,9 @@ from django.core.validators import RegexValidator
 
 
 class SubKreddit(models.Model):
-    user = models.ForeignKey(Kredditor, on_delete=models.CASCADE, null=True)
+    creator = models.ForeignKey(Kredditor, on_delete=models.CASCADE, null=True, related_name='creator')
     title = models.CharField(max_length=30)
     about = models.CharField(max_length=300)
     rules = models.CharField(max_length=300)
+    subscribers = models.ManyToManyField(Kredditor, related_name='subscribers', symmetrical=False, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
